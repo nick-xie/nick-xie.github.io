70bf220155d8da059e913957bf429225ef370d6d
Nick Xie <nickxie.me@gmail.com>
2018-01-24 00:13:35 -0500

    More photos and descriptions

--- main.js
+++ main.js
@@ -27,13 +27,31 @@ site.controller("mainController", function($scope, $window) {
 
   // Photos Carousel
   $scope.images = [
-    { num: 1, thumb: "images/slideshow/edited2.jpg", full: "images/slideshow/edited.jpg"},
-    { num: 2, thumb: "images/slideshow/bridge2.jpg", full: "images/slideshow/bridge.jpg"},
-    { num: 3, thumb: "images/slideshow/sunset2.jpg", full: "images/slideshow/sunset.jpg"},
-    { num: 4, thumb: "images/slideshow/mc2.jpg", full: "images/slideshow/mc.jpg"},
-    { num: 5, thumb: "images/slideshow/mcgreen2.jpg", full: "images/slideshow/mcgreen.jpg"},
-    { num: 6, thumb: "images/slideshow/kitchener2.jpg", full: "images/slideshow/kitchener1.jpg"},
-    { num: 7, thumb: "images/slideshow/path2.jpg", full: "images/slideshow/path.jpg"}
+    { thumb: "images/slideshow/bird2.jpg", full: "images/slideshow/bird.jpg",
+    desc: "This is a picture of a bird I took at the Biodome in Montreal. Quite a friendly chap"},
+    { thumb: "images/slideshow/cars2.jpg", full: "images/slideshow/cars.jpg",
+    desc: "Taken while on the road back from a Twilight track meet in Hamilton in Summer 2017." +
+    " Added some noise because I thought it looked cool."},
+    { thumb: "images/slideshow/hillside2.jpg", full: "images/slideshow/hillside.jpg",
+    desc: "Basic girl photo at Hillside Music Festival 2017. Performer pictured is Cœur de pirate."},
+    { thumb: "images/slideshow/hillsidesunset2.jpg", full: "images/slideshow/hillsidesunset.jpg",
+    desc: "Sunset at Hillside. Fun fact: I left this photo totally unedited! (serious)"},
+    { thumb: "images/slideshow/path2.jpg", full: "images/slideshow/path.jpg",
+    desc: "Some foggy morning walking from my dorm to class in my first year at Waterloo."},
+    { thumb: "images/slideshow/edited2.jpg", full: "images/slideshow/edited.jpg",
+    desc: "At Liujiazui in Shanghai. One of my favourite places in the whole world."},
+    { thumb: "images/slideshow/bridge2.jpg", full: "images/slideshow/bridge.jpg",
+    desc: "Went on a bike ride through Suzhou's alleys with some friends this day. We stopped by this bridge to" +
+    " admire the beauty."},
+    { thumb: "images/slideshow/sunset2.jpg", full: "images/slideshow/sunset.jpg",
+    desc: "Super nice sunset by the Boathouse in my hometown. I've spent many evenings here throughout my life."},
+    { thumb: "images/slideshow/mc2.jpg", full: "images/slideshow/mc.jpg",
+    desc: "Picture of the math building at Waterloo one night coming back from a midterm. Picture turned out great," +
+    " midterm did not."},
+    { thumb: "images/slideshow/mcgreen2.jpg", full: "images/slideshow/mcgreen.jpg",
+    desc: "Walking back to my dorm from the math building after doing some homework in my first term at Waterloo."},
+    { thumb: "images/slideshow/kitchener2.jpg", full: "images/slideshow/kitchener1.jpg",
+    desc: "Picture of some of Kitchener's distinctive historical building style."}
   ];
   $scope.photosNext = function() {
     $('.photos').slick('slickNext');
@@ -58,7 +76,8 @@ $(document).ready(function (){
   });
 
   $(".photos").slick({
-    arrows: false
+    arrows: false,
+    adaptiveHeight: true
   });
 });
 
